d66ee728b187d167237f0e3832949bce1caccb0c
Влад Таланов <111172960+VladTalanov@users.noreply.github.com>
2023-12-04 20:01:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Double> data = Conole.userData();
+        double result = Logic.indexCount(data.get(0), data.get(1));
+        Decorator.decorate(result);
+     }     
+}
